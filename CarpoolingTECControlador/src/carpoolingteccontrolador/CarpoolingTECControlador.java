eebe9964db335f9d0c56186bad3c52fc34b6eeed
jeja1 <jeja1@JEJAPC.mshome.net>
2019-06-03 22:47:43 -0600

    Creacion de la capa Controlador del proyecto

---
+++ CarpoolingTECControlador/src/carpoolingteccontrolador/CarpoolingTECControlador.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carpoolingteccontrolador;
+
+/**
+ *
+ * @author jeja1
+ */
+public class CarpoolingTECControlador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
